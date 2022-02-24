6cf75b2940e55fe096bf8d6c0459aeeb098cf7c9
EvanderGab <evander.gabriel@ti.ukdw.ac.id>
2022-02-24 15:22:27 +0700

    Revisi lat2

--- 71210794-03_Latihan2.py
+++ 71210794-03_Latihan2.py
@@ -1,3 +1,15 @@
+#3.2
+#Bentuk lama
+print("3.2")
+bilangan = int(input("Masukkan suatu bilangan: "))
+if bilangan > 0:
+    print("Positif")
+elif bilangan < 0:
+    print("Negatif")
+elif bilangan == 0:
+    print("Nol")
+
+#Bentuk baru
 bilangan = int(input("Masukkan suatu bilangan: "))
 
 print("Positif" if bilangan > 0 else "Negatif"
